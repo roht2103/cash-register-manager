38f445dba65d2f6f0b182ea22bf6d57ff74a6068
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-06-17 19:38:15 +0530

    changed codition

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let billAmount = document.querySelector("#bill-amount");
 let cashGiven = document.querySelector("#cash-given");
 const btn = document.querySelector("#checkBtn");
 const returnAmount = document.querySelector("#returnAmount");
-const noOfNotes=document.querySelectorAll(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 returnAmount.style.display = "none";
 
-const availableNotes=[2000,500,100,20,10,5,1];
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 btn.addEventListener("click", function () {
   // validateData() function will be called when button will be clicked
@@ -13,26 +13,26 @@ btn.addEventListener("click", function () {
 });
 
 function validateData() {
-  if (billAmount.value <= 0 || cashGiven.value <= 0) {
-    // Alert message when bill amount or cash given will be less than or equal to 0
-    alert("Invalid bill amount");
-  } else if (cashGiven.value < billAmount.value) {
-    //  Alert message when cash given is less than bill amount
-    alert("cash given is less than bill amount");
-  } else {
-    // else change will be calculated by calling calculateChange() function
+  if ((cashGiven.value - billAmount.value)>0) {
+    if (cashGiven.value <= 0 || billAmount.value < 0) {
+      alert("invalid credential");
+    } else {
+      // else change will be calculated by calling calculateChange() function
 
-    let amountToBeReturned = cashGiven.value - billAmount.value;
-    returnAmount.style.display = "block";
-    returnAmount.innerText = "Amount to be returned : " + amountToBeReturned;
-    calculateChange(amountToBeReturned);
+      let amountToBeReturned = cashGiven.value - billAmount.value;
+      returnAmount.style.display = "block";
+      returnAmount.innerText = "Amount to be returned : " + amountToBeReturned;
+      calculateChange(amountToBeReturned);
+    }
+  } else {
+    alert("cash given should be greater than bill amount");
   }
 }
 
 function calculateChange(amountToBeReturned) {
-    for(let i=0;i<availableNotes.length;i++){
-        const numberOfNotes=Math.trunc(amountToBeReturned/availableNotes[i]);
-        amountToBeReturned=amountToBeReturned%availableNotes[i];
-        noOfNotes[i].innerText=numberOfNotes;
-    }
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
