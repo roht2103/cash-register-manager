d09a2e2de9b19dd548fd213592c578dc1d33266d
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-06-17 15:32:41 +0530

    added padding to input box

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ let cashGiven = document.querySelector("#cash-given");
 const btn = document.querySelector("#checkBtn");
 
 btn.addEventListener("click", function () {
-    console.log("clicked");
-    console.log(billAmount.value);
     if(cashGiven.value<billAmount.value){
         alert("cash given is less than bill amount");
     }
