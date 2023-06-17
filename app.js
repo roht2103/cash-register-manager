f7247ac18c363f513c00a5ee960ea79fedca4502
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-06-17 16:48:43 +0530

    validate data

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ let cashGiven = document.querySelector("#cash-given");
 const btn = document.querySelector("#checkBtn");
 
 btn.addEventListener("click", function () {
-    if(cashGiven.value<billAmount.value){
+
+    validateData();
+
+    
+});
+
+function validateData(){
+    if(billAmount.value<=0 || cashGiven.value<=0){
+        alert("Invalid bill amount");
+    }
+    else if(cashGiven.value<billAmount.value){
         alert("cash given is less than bill amount");
     }
-});
+    else{
+        let amountToBeReturned=cashGiven.value-billAmount.value;
+        calculateChange();
+    }
+}
+
+function calculateChange(amountToBeReturned){
+
+}
